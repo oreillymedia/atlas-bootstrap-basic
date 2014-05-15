5d7fa057962d6619f383d5f7f411da943cae764c
Andrew Odewahn <odewahn@oreilly.com>
2014-05-15 16:08:47 -0400

    tiny tweaks

--- app.rb
+++ app.rb
@@ -4,20 +4,24 @@ require 'liquid'
 
 class App < Sinatra::Base
 
+  # Use this data structure to pass values into the liquid template
+  # The hash keys should match values in atlas.json
+  @@atlas_data = {
+    'layout'    => "theme/html/layout.html",
+    'title'     => "Bootstrap theme test",
+    'doctype'   => "<html lang='en' xmlns='http://www.w3.org/1999/xhtml'>",
+    'content'   => IO.read("sample-content.html"),
+    'toc'       => IO.read("sample-toc.html"),
+    'prev_link' => "Previous",
+    'next_link' => "Next"
+  }
+  
+  # route to the index file
   get "/" do
-
-    atlas_data = {
-      'title' => "Bootstrap theme test",
-      'doctype' => "<html lang='en' xmlns='http://www.w3.org/1999/xhtml'>",
-      'content' => IO.read("sample-content.html"),
-      'toc' => IO.read("sample-toc.html"),
-      'prev_link' => "Previous",
-      'next_link' => "Next"
-    }
-
-    @template = Liquid::Template.parse(IO.read("theme/html/layout.html"))
-    @template.render(atlas_data)
-    
+    template = Liquid::Template.parse(IO.read(@@atlas_data['layout']))
+    template.render(@@atlas_data)
   end
+  
+  
   
 end
